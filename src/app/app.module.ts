f3eba3676ec7db7d8f343c3768f3ef66697144a6
Ryan Borja <ryan.t.borja@gmail.com>
2022-12-01 02:01:47 -0500

    add material design and rid of boilerplate

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { SuperheroTabsComponent } from './superhero-tabs/superhero-tabs.component';
+import { MaterialExampleModule } from '../material.module';
+import { ToolbarTopComponent } from './toolbar-top/toolbar-top.component';
+import { SuperheroListComponent } from './superhero-list/superhero-list.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SuperheroTabsComponent,
+    ToolbarTopComponent,
+    SuperheroListComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MaterialExampleModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
